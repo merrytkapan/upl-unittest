709812399f95b83c166ee1c324798744b51b9205
merrytkapan <merrytantob@gmail.com>
2022-03-20 22:02:43 +0700

    Change parameter formal

--- upl-unittest/src/main/java/id/ifundip/calculator/Calculator.java
+++ upl-unittest/src/main/java/id/ifundip/calculator/Calculator.java
@@ -1,8 +1,8 @@
 package id.ifundip.calculator;
 
 public class Calculator {
-	public int addNumber(int num1, int num2) {
-		return num1 + num2;
+	public int addNumber(int a, int b) {
+		return a + b;
 	}
 	
 	public int subtractNumber(int num1, int num2) {
